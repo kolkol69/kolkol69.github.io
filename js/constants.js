7b87d864e0c296bf4f2a1eb6084fb01699d0c083
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2018-12-28 12:59:12 +0100

    moved changes from the parent repo, short descr: added targets, algoritm now work a little differently, obstacles are now supported and are represented as a single obstacle(which is now also a target)

--- js/constants.js
+++ js/constants.js
@@ -3,7 +3,7 @@
 ## AGENTS' DIMENSIONS ##
 ########################
 */
-const HEIGHT = 10 ;
+const HEIGHT = 10;
 const WIDTH = 3;
 const DEPTH = 3;
 
@@ -20,8 +20,9 @@ const INIT_POS_Z = GROUND_HEIGHT / 2;
 // camera params
 const CAMERA_ANGLE = 1.57;
 const CAMERA_ZOOM = 500;
-const CAMERA_POS_X = 300;
-const CAMERA_POS_Y = 200;
+const CAMERA_POS_X = INIT_POS_X;
+const CAMERA_POS_Y = INIT_POS_Z;
+const CAMERA_VIEW_TYPE = -1; // 1 - normal view; -1 - from the top view
 
 /* 
 #######################
@@ -31,11 +32,11 @@ const CAMERA_POS_Y = 200;
 // agent amount
 let agentsAmount = 100;
 // Promień sąsiedztwa
-let neighbourRadius = 50.0;
+let neighbourRadius = 11.0;
 // Kąt obserwacji
 let observDegree = 120.0;
 // Odleglosc minimalna
-let minDistance = 20.0;
+let minDistance = 11.0;
 // Waga prędkości sąsiadów
 let weightNeighbourVelocity = 0.1;
 // Waga odległości sąsiadów
@@ -45,4 +46,86 @@ let weightMinimalDistance = 0.15;
 // Waga zakłóceń
 let weightPerturbation = 0.1;
 // Prędkość maksymalna
-let maxVelocity = 2.0;
+let maxVelocity = 1.0;
+// defines how fast agents move towards the target
+let speedToTarget = 1.0;
+// defines how fast agents will get to the target
+let timeToTarget = 3000; // time in ms
+// defines how likely most of the agents will get to the target
+let chanceToGetToTarget = 3; // example: 100 / 3 ~~ 33.3%
+/* 
+##########################
+### OBSTACLE POSITIONS ###
+##########################
+*/
+
+const OBSTACLE_POSITIONS = [
+    // {
+    //         x: 310,
+    //         y: 200,
+    //         width: 20,
+    //         depth: 150,
+    //     }, {
+    //         x: 270,
+    //         y: 200,
+    //         width: 20,
+    //         depth: 150,
+    //     },
+    //     {
+    //         x: 150,
+    //         y: 70,
+    //         width: 100,
+    //         depth: 10,
+    //     }, {
+    //         x: 60,
+    //         y: 150,
+    //         width: 50,
+    //         depth: 100,
+    //     }, {
+    //         x: 290,
+    //         y: 300,
+    //         width: 50,
+    //         depth: 10,
+    //     }, 
+    {
+        x: 300,
+        y: 200,
+        width: 10,
+        depth: 10,
+    }
+]
+const TARGET_POSITIONS = [
+    // {
+    //         x: 310,
+    //         y: 200,
+    //         width: 20,
+    //         depth: 150,
+    //     }, {
+    //         x: 270,
+    //         y: 200,
+    //         width: 20,
+    //         depth: 150,
+    //     },
+    //     {
+    //         x: 150,
+    //         y: 70,
+    //         width: 100,
+    //         depth: 10,
+    //     }, {
+    //         x: 60,
+    //         y: 150,
+    //         width: 50,
+    //         depth: 100,
+    //     }, {
+    //         x: 290,
+    //         y: 300,
+    //         width: 50,
+    //         depth: 10,
+    //     }, 
+    {
+        x: 300,
+        y: 200,
+        width: 10,
+        depth: 10,
+    }
+]
--- js/setup/sceneSetup.js
+++ js/setup/sceneSetup.js
@@ -6,10 +6,36 @@ const createAgentMeshes = (scene) => {
             width: WIDTH,
             depth: DEPTH
         }, scene);
-        agents[i].position.y = 5;
+        agents[i].position.y = HEIGHT/2;
     }
     return agents;
 }
+
+// const createTouristPlaces = (scene) => {
+//     const place = BABYLON.MeshBuilder.CreateBox("", {
+//         height: HEIGHT*2,
+//         width: OBSTACLE_POSITIONS[0].width,
+//         depth: OBSTACLE_POSITIONS[0].depth
+//     }, scene);
+//     place.position.y = HEIGHT;
+//     place.position.x = OBSTACLE_POSITIONS[0].x;
+//     place.position.z = OBSTACLE_POSITIONS[0].y;
+// }
+
+const createTouristPlaces = (scene) => {
+    OBSTACLE_POSITIONS.forEach((obstacle)=>{
+        const place = BABYLON.MeshBuilder.CreateBox("", {
+            height: HEIGHT*2,
+            width: obstacle.width,
+            depth: obstacle.depth
+        }, scene);
+        place.position.y = HEIGHT;
+        place.position.x = obstacle.x;
+        place.position.z = obstacle.y;
+    });
+}
+
+
 
 const removeAgentMeshes = () => {
     for (let i = 0; i < agents.length; i++) {
